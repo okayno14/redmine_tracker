fb74a4f05653fcd031009a40cef51008503d6cef
aleksandr alekseev <okayno14@gmail.com>
2026-04-08 12:17:35 +0700

    draft: Added to controller export_to_csv

--- src/redmine_tracker_controller.erl
+++ src/redmine_tracker_controller.erl
@@ -2,8 +2,15 @@
 
 -behaviour(controller).
 
+-include_lib("kernel/include/logger.hrl").
+
 -export([route/1]).
 
-route(_) ->
+route(#{request := <<"export_to_csv">>}) ->
+    %% TODO надо отлавливать ошибку транзакции
+    {atomic, CSV} = track_composed:export_to_csv(),
+    response:ok_response(CSV);
+route(_UnknownReq) ->
+    ?LOG_ERROR("Unkown Req:~p", [_UnknownReq]),
     response:ok_response(<<"ok">>).
 
