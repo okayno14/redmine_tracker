773a143945285f420f27a970070bd96702dd20c9
aleksandr alekseev <okayno14@gmail.com>
2026-04-12 12:51:57 +0700

    redmine_tracker_controller refactored

--- src/redmine_tracker_controller.erl
+++ src/redmine_tracker_controller.erl
@@ -6,11 +6,42 @@
 
 -export([route/1]).
 
+-eqwalizer({nowarn_function, process_transaction/2}).
+
 route(#{request := <<"export_to_csv">>}) ->
-    %% TODO надо отлавливать ошибку транзакции
-    {atomic, CSV} = track_composed:export_to_csv(),
-    response:ok_response(CSV);
+    process_transaction(
+        fun() ->
+            track_composed:export_to_csv()
+        end,
+        fun
+            ({atomic, CSV}) ->
+                response:ok_response(CSV);
+            (_) ->
+                nomatch
+        end
+    );
 route(_UnknownReq) ->
     ?LOG_ERROR("Unkown Req:~p", [_UnknownReq]),
     response:ok_response(<<"ok">>).
+
+process_transaction(Transaction, HappyPath) ->
+    ErrorResponse = fun
+        ({aborted, {Reason, ST}}) when is_list(ST) ->
+            response:error_response(
+                transaction_failed,
+                unicode:characters_to_binary(
+                    erl_error:format_exception(error, Reason, ST)
+                )
+            );
+        ({aborted, Reason}) ->
+            response:error_response(
+                transaction_failed,
+                unicode:characters_to_binary(io_lib:format("~p", [Reason]))
+            )
+    end,
+    Ret = Transaction(),
+    case HappyPath(Ret) of
+        nomatch -> ErrorResponse(Ret);
+        #{type := ok} = OK -> OK
+    end.
 
