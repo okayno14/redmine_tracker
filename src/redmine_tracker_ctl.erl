82a7fb030f0ab5a9d411c1dbf8366d1c1b40074c
aleksandr alekseev <okayno14@gmail.com>
2026-04-15 01:05:16 +0700

    Added export_to_csv prototype to redmine_tracker_ctl

--- src/redmine_tracker_ctl.erl
+++ src/redmine_tracker_ctl.erl
@@ -3,10 +3,6 @@
 %% API exports
 -export([main/1]).
 
-%%====================================================================
-%% API functions
-%%====================================================================
-
 %% escript Entry point
 main(Args) ->
     argparse:run(Args, cli(), #{progname => ?MODULE}).
@@ -17,7 +13,7 @@ cli() ->
         commands => #{
             "export_to_csv" => #{
                 arguments => [],
-                handler => fun(L) -> io:format("Args: ~p\n", [L]) end
+                handler => fun export_to_csv/1
             },
             "import_from_csv" => #{
                 arguments => [
@@ -32,3 +28,23 @@ cli() ->
             }
         }
     }.
+
+export_to_csv(#{}) ->
+    {ok, Resp} = send_req(json:encode(#{request => <<"export_to_csv">>})),
+    #{<<"type">> := <<"ok">>, <<"data">> := CSV} = Resp,
+    io:format("~ts\n", [CSV]).
+
+send_req(Req) -> send_req(<<"/tmp/redmine_tracker.sock">>, Req).
+%% TODO переписать на either с отметками о месте возникновения ошибки,
+%% чтобы потом можно было прикрутить для ошибки дефолтный обработчик,
+%% а внутри функций обрабатывать resp
+%% TODO в обработчике Resp нужно также сделать кастомный код и общий код для дефолтных ошибок - стактрейсов и т.д.
+send_req(Path, Req) ->
+    maybe
+        {ok, Socket} ?= gen_tcp:connect({local, Path}, 0, [{active, false}, binary]),
+        ok ?= gen_tcp:send(Socket, Req),
+        {ok, Bin} ?= gen_tcp:recv(Socket, 0),
+        true ?= is_binary(Bin),
+        {ok, json:decode(Bin)}
+    end.
+
