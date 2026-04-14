e46f8971c52528ddfef31deff3a2c762abae0bbc
aleksandr alekseev <okayno14@gmail.com>
2026-04-14 11:49:10 +0700

    Added simple argument parsing to redmine_tracker_ctl

--- src/redmine_tracker_ctl.erl
+++ src/redmine_tracker_ctl.erl
@@ -9,6 +9,26 @@
 
 %% escript Entry point
 main(Args) ->
-    io:format("Args: ~p~n", [Args]),
-    erlang:halt(0).
+    argparse:run(Args, cli(), #{progname => ?MODULE}).
 
+cli() ->
+    #{
+        arguments => [],
+        commands => #{
+            "export_to_csv" => #{
+                arguments => [],
+                handler => fun(L) -> io:format("Args: ~p\n", [L]) end
+            },
+            "import_from_csv" => #{
+                arguments => [
+                    #{
+                        name => csv,
+                        default => <<"-">>,
+                        type => binary,
+                        help => <<"\"-\" - stdin or \"Path\" - path to csv-file">>
+                    }
+                ],
+                handler => fun(L) -> io:format("Args: ~p\n", [L]) end
+            }
+        }
+    }.
