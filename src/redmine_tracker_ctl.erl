1aedd60de08bd337268a159e3af53c848f0267d8
aleksandr alekseev <okayno14@gmail.com>
2026-04-15 14:12:34 +0700

    Added import_from_csv to cli

--- src/redmine_tracker_ctl.erl
+++ src/redmine_tracker_ctl.erl
@@ -24,7 +24,7 @@ cli() ->
                         help => <<"\"-\" - stdin or \"Path\" - path to csv-file">>
                     }
                 ],
-                handler => fun(L) -> io:format("Args: ~p\n", [L]) end
+                handler => fun import_from_csv/1
             }
         }
     }.
@@ -34,6 +34,22 @@ export_to_csv(#{}) ->
     #{<<"type">> := <<"ok">>, <<"data">> := CSV} = Resp,
     io:format("~ts\n", [CSV]).
 
+import_from_csv(#{csv := <<"-">>}) ->
+    CSV = unicode:characters_to_binary(read_all_io(standard_io, [])),
+    {ok, Resp} = send_req(json:encode(#{request => <<"import_from_csv">>, csv => CSV})),
+    #{<<"type">> := <<"ok">>, <<"data">> := Data} = Resp,
+    io:format("~ts\n", [Data]);
+import_from_csv(Args = #{csv := _File}) ->
+    io:format("Args: ~p\n", [Args]).
+
+read_all_io(Device, Acc) ->
+    io:setopts(standard_io, [binary]),
+    case io:get_line(Device, "") of
+        eof -> lists:reverse(Acc);
+        String -> read_all_io(Device, [String | Acc])
+    end.
+
+%% TODO попробовать вытащить из конфиги
 send_req(Req) -> send_req(<<"/tmp/redmine_tracker.sock">>, Req).
 %% TODO переписать на either с отметками о месте возникновения ошибки,
 %% чтобы потом можно было прикрутить для ошибки дефолтный обработчик,
