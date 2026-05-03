aa5028729546c013beb852a62f4bd0ea7de14da2
aleksandr alekseev <okayno14@gmail.com>
2026-05-04 00:56:02 +0700

    Added json2:encode/2

--- src/unix_socket_framework/json2.erl
+++ src/unix_socket_framework/json2.erl
@@ -1,12 +1,24 @@
 -module(json2).
 
 -export([
+    encode/2,
     decode/1
 ]).
 
 -export_type([
     decode_reason/0
 ]).
+
+-spec encode(json:encode_value(), Format :: oneline | indent) ->
+    unicode:unicode_binary().
+encode(Term, indent) ->
+    Bin = unicode:characters_to_binary(json:format(Term)),
+    true = is_binary(Bin),
+    Bin;
+encode(Term, oneline) ->
+    Bin = unicode:characters_to_binary(json:encode(Term)),
+    true = is_binary(Bin),
+    Bin.
 
 -type decode_reason() ::
     bad_response
