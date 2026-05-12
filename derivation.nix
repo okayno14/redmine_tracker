a6d086e4822029ec23d2f880205cedf7ba6d5985
aleksandr alekseev <okayno14@gmail.com>
2026-05-12 12:46:44 +0700

    Removed empty code from derivation

--- derivation.nix
+++ derivation.nix
@@ -4,15 +4,7 @@ let
     }) {};
 in
   with pkgs_otp27;
-    let
-      # TODO use common map and inherit keys
-      # deps = fetchRebar3Deps {
-      #   name = "redmine_tracker";
-      #   version = "0.0.1";
-      #   src = ./.;
-      #   sha256 = "0000000000000000000000000000000000000000000000000000";
-      # };
-    in
+    (
       beam27Packages.rebar3Relx {
         name = "redmine_tracker";
         pname = "redmine_tracker";
@@ -21,33 +13,17 @@ in
         releaseType = "release";
         src = ./.;
         checkouts =
-          pkgs_otp27.lib.traceVal
-            callPackage ./fetch-rebar3-deps.nix {} {
-              # TODO убрать копипасту
-              name = "redmine_tracker";
-              version = "0.0.1";
-              src = ./.;
-              sha256 = "sha256-plUDn1sKZKlgcw0q5kpkhtxs2ifN50lyBpjgdpi3lZY=";
-              nativeBuildInputs = [ git ];
-            };
-        # nativeBuildInputs = [
-        #   beam27Packages.erlang
-        #   beam27Packages.rebar3
-        # ];
-        # buildInputs = [
-        #   beam27Packages.erlang
-        # ];
-        # buildPhase = ''
-        #   # echo $USER
-        #   # echo $PWD
-        #   # export HOME=$TMPDIR
-        #   # export DEBUG=1
-        #   # rebar3 get-deps
-        #   # rebar3 compile
-        #   rebar3 release
-        # '';
-        # deps = beam27Packages.fetchRebar3Deps {
-        #   rebarLock = ./rebar.lock;
-        # };
+          callPackage ./fetch-rebar3-deps.nix {} {
+            # TODO убрать копипасту
+            name = "redmine_tracker";
+            version = "0.0.1";
+            src = ./.;
+            sha256 = "sha256-plUDn1sKZKlgcw0q5kpkhtxs2ifN50lyBpjgdpi3lZY=";
+            nativeBuildInputs = [ git ];
+          };
       }
-
+    ).overrideAttrs (old: {
+      postInstall = ''
+        echo "Deps fetched to $checkouts"
+      '';
+    })
