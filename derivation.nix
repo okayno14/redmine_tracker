f5acd5e86f5493653cab9adebb3739b79ccbbcce
aleksandr alekseev <okayno14@gmail.com>
2026-05-12 12:50:38 +0700

    Removed code duplication

--- derivation.nix
+++ derivation.nix
@@ -2,22 +2,20 @@ let
     pkgs_otp27 = import (builtins.fetchTarball {
           url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
     }) {};
+    name = "redmine_tracker";
+    pname = name;
+    version = "0.0.1";
+    src = ./.;
 in
   with pkgs_otp27;
     (
       beam27Packages.rebar3Relx {
-        name = "redmine_tracker";
-        pname = "redmine_tracker";
-        version = "0.0.1";
+        inherit pname version src;
         profile = "prod";
         releaseType = "release";
-        src = ./.;
         checkouts =
           callPackage ./fetch-rebar3-deps.nix {} {
-            # TODO убрать копипасту
-            name = "redmine_tracker";
-            version = "0.0.1";
-            src = ./.;
+            inherit name version src;
             sha256 = "sha256-plUDn1sKZKlgcw0q5kpkhtxs2ifN50lyBpjgdpi3lZY=";
             nativeBuildInputs = [ git ];
           };
