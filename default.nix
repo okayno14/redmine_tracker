953108cbb2f63e3b6564740a5cad7404696dc628
aleksandr alekseev <okayno14@gmail.com>
2026-07-27 09:13:44 +0700

    default.nix - added callPackage

--- default.nix
+++ default.nix
@@ -2,17 +2,11 @@ let
   pkgs_otp27 = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
   }) { };
+  deps = pkgs_otp27.callPackage ./deps2.nix { };
   name = "redmine_tracker";
   pname = name;
   version = "0.0.2";
   src = ./.;
-
-  deps = import ./deps2.nix {
-    stdenv = pkgs_otp27.stdenv;
-    rebar3 = pkgs_otp27.rebar3;
-    git = pkgs_otp27.git;
-    cacert = pkgs_otp27.cacert;
-  };
 in
 with pkgs_otp27;
 (
