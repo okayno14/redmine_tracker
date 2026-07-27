7febbdb98113dbc6a80d4560f1b32f7021456f39
aleksandr alekseev <okayno14@gmail.com>
2026-07-27 09:10:30 +0700

    default - nixfmt + removed useless comments

--- default.nix
+++ default.nix
@@ -1,63 +1,38 @@
 let
-    pkgs_otp27 = import (builtins.fetchTarball {
-          url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
-    }) {};
-    name = "redmine_tracker";
-    pname = name;
-    version = "0.0.2";
-    src = ./.;
+  pkgs_otp27 = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+  }) { };
+  name = "redmine_tracker";
+  pname = name;
+  version = "0.0.2";
+  src = ./.;
 
-    # Refactor: looks bad
-    # deps = import ./deps.nix {
-    #   lib = pkgs_otp27.lib;
-    #   stdenvNoCC = pkgs_otp27.stdenvNoCC;
-    #   rebar3 = pkgs_otp27.rebar3;
-    #   git = pkgs_otp27.git;
-    #   coreutils = pkgs_otp27.coreutils;
-    # };
-    deps = import ./deps2.nix {
-      stdenv = pkgs_otp27.stdenv;
-      rebar3 = pkgs_otp27.rebar3;
-      git = pkgs_otp27.git;
-      cacert = pkgs_otp27.cacert;
-    };
-    # rebar3 = pkgs_otp27.beam27Packages.rebar3.overrideAttrs(
-    #   old: {nativeBuildInputs = [pkgs_otp27.git];}
-    # );
+  deps = import ./deps2.nix {
+    stdenv = pkgs_otp27.stdenv;
+    rebar3 = pkgs_otp27.rebar3;
+    git = pkgs_otp27.git;
+    cacert = pkgs_otp27.cacert;
+  };
 in
-  with pkgs_otp27;
-    (
+with pkgs_otp27;
+(
 
-      # (beam27Packages.fetchRebar3Deps {
-      #   inherit name version src;
-      #   sha256 = "sha256-jv0kVdpsaISvBF60PYZXw0QGyXHcrJyqLB83Ptv8kMw=";
-      # }).overrideAttrs (old: {
-      #   nativeBuildInputs = [ git ];
-      # })
-
-      # .overrideAttrs (old: {
-          # preBuild = (old.preBuild or "") + ''
-          #   export PATH="${git}/bin:$PATH"
-          # '';
-          # postInstall = ''
-          #   echo "Deps fetched to $out"
-          # '';
-      # })
-      beam27Packages.rebar3Relx {
-        inherit pname version src;
-        profile = "prod";
-        releaseType = "release";
-        checkouts =
-          (deps {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./rebar.lock
-                  ./rebar.config
-                ];
-              };
-              inherit name version;
-              sha256 = "sha256-WRVHCVcVdA/qpTNHbSehNsC7UKGBI/DYtSEOWa3htkQ=";
-          });
+  beam27Packages.rebar3Relx {
+    inherit pname version src;
+    profile = "prod";
+    releaseType = "release";
+    checkouts = (
+      deps {
+        src = lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./rebar.lock
+            ./rebar.config
+          ];
+        };
+        inherit name version;
+        sha256 = "sha256-WRVHCVcVdA/qpTNHbSehNsC7UKGBI/DYtSEOWa3htkQ=";
       }
-    )
+    );
+  }
+)
